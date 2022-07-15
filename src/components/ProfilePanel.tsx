ab0d459fd4e435b15b8bdd980a121dbcbf8b6db3
Tauhoo <tauhoo_ice@hotmail.com>
2022-07-15 17:57:17 +0700

    set up profile content

--- src/components/ProfilePanel.tsx
+++ src/components/ProfilePanel.tsx
@@ -12,7 +12,7 @@ const ContentLayout = styled.div`
   height: 100%;
   display: grid;
   grid-template-rows: max-content 1fr max-content;
-  gap: 30px;
+  gap: 20px;
 `
 
 const PanelProfile = () => {
@@ -27,7 +27,7 @@ const PanelProfile = () => {
             pages={[
               <>
                 <PanelImage url={profileImage}></PanelImage>
-                <div>
+                <div style={{ marginTop: '20px' }}>
                   I’m Wachirawit Wacharak. You can call me Ice. I’m working as
                   Software Engineer at KBTG in Bangkok. Although, My main skill
                   is web development both frontend and backend, I’m still
@@ -35,21 +35,24 @@ const PanelProfile = () => {
                 </div>
               </>,
               <>
-                <PanelImage url={profileImage}></PanelImage>
                 <div>
-                  I’m Wachirawit Wacharak. You can call me Ice. I’m working as
-                  Software Engineer at KBTG in Bangkok. Although, My main skill
-                  is web development both frontend and backend, I’m still
-                  interested in art and others.
+                  What I’m learning
+                  <br />
+                  - 3D modeling on Blender
+                  <br />
+                  - I’m currently study how to modeling and shading in Blender.
+                  <br />
+                  - Piano - I’m also interest in music creation. So, I think, I
+                  will start with piano as my first instrument.
+                  <br />- English - My speaking and listening pretty bad. So, I
+                  think it time to improve it seriously.
                 </div>
               </>,
               <>
-                <PanelImage url={profileImage}></PanelImage>
                 <div>
-                  I’m Wachirawit Wacharak. You can call me Ice. I’m working as
-                  Software Engineer at KBTG in Bangkok. Although, My main skill
-                  is web development both frontend and backend, I’m still
-                  interested in art and others.
+                  What I like
+                  <br />- Playing game - I’m play game with my friend every
+                  Friday and Saturday such as Valorant and Dota2.
                 </div>
               </>,
             ]}
